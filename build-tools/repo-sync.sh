e6c7e207b176e6991e84dc54d5cedaf581c27231
CH4rnel <frankmozes777lucky@gmail.com>
2026-09-22 02:58:52 +0300

    style: fix SC1128 shebang placement and fix SC2035 (Safe globbing)

--- build-tools/repo-sync.sh
+++ build-tools/repo-sync.sh
@@ -4,6 +4,7 @@
 
 set -e
 
+# shellcheck disable=SC1091
 source "$(dirname "$0")/build.conf"
 
 mkdir -p "$CH4RCH_REPO/$CH4RCH_ARCH"
@@ -13,15 +14,15 @@ cp -f "$CH4RCH_PKGDEST"/*.pkg.tar.zst* "$CH4RCH_REPO/$CH4RCH_ARCH/" 2>/dev/null 
 
 cd "$CH4RCH_REPO/$CH4RCH_ARCH"
 
-rm -f *.db.tar.zst
-rm -f *.files.tar.zst
+rm -f ./*.db.tar.zst
+rm -f ./*.files.tar.zst
 
 echo "[CH4RCH] Updating repository database..."
-repo-add -s -v "${CH4RCH_REPO_NAME}.db.tar.zst" *.pkg.tar.zst
+repo-add -s -v "${CH4RCH_REPO_NAME}.db.tar.zst" ./*.pkg.tar.zst
 
 # Log the repository update to the hash-chain
 if [ -x "$CH4RCH_SRC/build-tools/hash-chain.sh" ]; then
-    PKG_COUNT=$(ls -1 *.pkg.tar.zst 2>/dev/null | wc -l)
+    PKG_COUNT=$(find . -maxdepth 1 -name "*.pkg.tar.zst" | wc -l)
     "$CH4RCH_SRC/build-tools/hash-chain.sh" "REPO_SYNC arch=${CH4RCH_ARCH} repo=${CH4RCH_REPO_NAME} packages=${PKG_COUNT}"
 else
     echo "[CH4RCH] WARNING: hash-chain.sh not found, audit logging skipped."
